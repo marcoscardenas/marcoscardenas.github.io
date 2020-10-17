3ae1742de8219aff50e0e4f47d35b98a932a9d36
marcoscardenas <car20092@byui.edu>
2020-10-16 23:37:28 -0600

    progress

--- poundstokilograms.js
+++ poundstokilograms.js
@@ -1,16 +1,22 @@
 function addNumbers() {
     // INPUT
     let number1 = document.getElementById("number1").value;
-    let number2 = document.getElementById("number2").value;
+    
     // PROCESSING
     //	convert the input strings to numbers using parafloat;
     
+    
 
     number1 = parseFloat(number1);
-    number2 = parseFloat(number2);
+    number2 = 0.454;
+    
+    
     // add the tow nmubers together and store in my output variable
-    let sum = number1 + number2;
+    let sum = number1 * number2;
 
     // OUTPUT
-    document.getElementById("sum").innerHTML = "The sum <strong>is</strong> " + sum + "!";
+    document.getElementById("Kilogram").innerHTML = "The sum <strong>is</strong> " + sum+ "!";
+    
+    
   }
+
